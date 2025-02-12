72f0f7f593d16172c0dacb09d58684d90cde84ed
Youssef Mazini <maziniyoussef29@gmail.com>
2025-02-12 17:17:42 +0100

    1+1=3

---
+++ Train.c
@@ -0,0 +1,29 @@
+
+
+// This file only for train on the new functions each exercice and each concept behind it; and where come from in breif and some commpn Q&A and each has main function of course;
+
+// 1/ Understanding Error Handling "perror and strerror"
+
+// 2/ File Access and Manipulation "access and unlink"
+
+// 3/  Duplicating File Descriptors "dup and dup2"
+
+// 4/ Process Creation and Execution "fork and execve"
+
+// 5/ Inter-Process Communication "pipe and heredoc 'ONLY IDEA AND HOW it works' " 
+
+// 6/ Process Termination and Waiting "exit, wait, waitpid"
+
+// 7/ Integration - Building a Mini Pipex 
+		/*
+			Mini Pipex Implementation
+    	Exercise:
+     	   Implement ./pipex infile "cat" "wc -l" outfile.
+     	   Use fork(), pipe(), dup2(), execve(), and waitpid().
+		*/
+
+// 8/ Summary of all to Recap;
+
+// 9/ 
+
+// 10/ 
