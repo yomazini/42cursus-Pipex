80656d95425d92479a4a9f754ccef5cf181fcac4
Youssef Mazini <maziniyoussef29@gmail.com>
2025-02-18 11:05:34 +0100

    norms 2.0| more tests must be added

--- src_bonus/pipex_bonus.c
+++ src_bonus/pipex_bonus.c
@@ -6,11 +6,26 @@
 /*   By: ymazini <ymazini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 14:24:20 by ymazini           #+#    #+#             */
-/*   Updated: 2025/02/18 10:52:02 by ymazini          ###   ########.fr       */
+/*   Updated: 2025/02/18 11:03:58 by ymazini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+int	open_outfile(char *path, int hdoc)
+{
+	int	fd;
+	int	flags;
+
+	if (hdoc)
+		flags = O_WRONLY | O_CREAT | O_APPEND;
+	else
+		flags = O_WRONLY | O_CREAT | O_TRUNC;
+	fd = open(path, flags, 0644);
+	if (fd < 0)
+		(perror("pipex: outfile"), exit(1));
+	return (fd);
+}
 
 static void	pipe_chain(char *cmd, int *prev_pipe, char **env)
 {
@@ -68,6 +83,22 @@ pid_t	handle_multipipe(int ac, char **av, char **env, int hdoc)
 	return (-1);
 }
 
+static void	handle_input(char **av, int *in_fd, int hdoc)
+{
+	if (hdoc)
+	{
+		handle_hdoc(av[2], in_fd);
+		(dup2(*in_fd, 0), close(*in_fd));
+	}
+	else
+	{
+		*in_fd = open(av[1], O_RDONLY);
+		if (*in_fd < 0)
+			(perror(av[1]), exit(1));
+		(dup2(*in_fd, 0), close(*in_fd));
+	}
+}
+
 int	main(int ac, char **av, char **env)
 {
 	int		in_fd;
@@ -78,18 +109,7 @@ int	main(int ac, char **av, char **env)
 	if (ac < 5 + (ft_strncmp(av[1], "here_doc", 9) == 0))
 		(ft_putstr_fd("Invalid number of arguments\n", 2), exit(1));
 	hdoc = (ft_strncmp(av[1], "here_doc", 9) == 0);
-	if (hdoc)
-	{
-		handle_hdoc(av[2], &in_fd);
-		(dup2(in_fd, 0), close(in_fd));
-	}
-	else
-	{
-		in_fd = open(av[1], O_RDONLY);
-		if (in_fd < 0)
-			(perror(av[1]), exit(1));
-		(dup2(in_fd, 0), close(in_fd));
-	}
+	handle_input(av, &in_fd, hdoc);
 	last_pid = handle_multipipe(ac, av, env, hdoc);
 	waitpid(last_pid, &status, 0);
 	while (wait(NULL) > 0)
--- src_bonus/tools_bonus.c
+++ src_bonus/tools_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ymazini <ymazini@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ymazini <ymazini@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 14:24:25 by ymazini           #+#    #+#             */
-/*   Updated: 2025/02/18 10:45:16 by ymazini          ###   ########.fr       */
+/*   Updated: 2025/02/18 10:57:28 by ymazini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,28 @@ static char	**parse_cmd(char *cmd)
 	return (args);
 }
 
+static char	*find_command_path(char **args, char **env)
+{
+	char	**paths;
+	char	*path;
+
+	if (access(args[0], X_OK) == 0 && ft_strchr(args[0], '/'))
+		return (args[0]);
+	paths = get_env_arr(env);
+	path = get_path(paths, args[0]);
+	free_all(paths);
+	return (path);
+}
+
 void	exec_cmd(char *cmd, char **env)
 {
 	char	**args;
 	char	*path;
-	char	**paths;
 
 	args = parse_cmd(cmd);
 	if (!args || !args[0])
 		(ft_putstr_fd("pipex: command not found\n", 2), exit(127));
-	if (access(args[0], X_OK) == 0 && ft_strchr(args[0], '/'))
-		path = args[0];
-	else
-	{
-		paths = get_env_arr(env);
-		path = get_path(paths, cmd);
-		free_all(paths);
-	}
+	path = find_command_path(args, env);
 	if (!path)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
@@ -56,11 +61,27 @@ void	exec_cmd(char *cmd, char **env)
 	}
 }
 
+static void	process_heredoc(int fd, char *expected)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = get_next_line(0);
+		if (!line || ft_strncmp(line, expected, ft_strlen(line)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(fd, line, ft_strlen(line));
+		free(line);
+	}
+}
+
 void	handle_hdoc(char *limiter, int *prev_pipe)
 {
 	int		fd[2];
 	pid_t	pid;
-	char	*line;
 	char	*expected;
 
 	expected = ft_strjoin(limiter, "\n");
@@ -72,41 +93,13 @@ void	handle_hdoc(char *limiter, int *prev_pipe)
 	if (pid == 0)
 	{
 		close(fd[0]);
-		while (1)
-		{
-			line = get_next_line(0);
-			if (!line || ft_strncmp(line, expected, ft_strlen(line)) == 0) 
-			{
-				free(line);
-				break ;
-			}
-			write(fd[1], line, ft_strlen(line));
-			free(line);
-		}
+		process_heredoc(fd[1], expected);
 		close(fd[1]);
 		free(expected);
 		exit(0);
 	}
-	else
-	{
-		close(fd[1]);
-		*prev_pipe = fd[0];
-		waitpid(pid, NULL, 0);
-	}
+	close(fd[1]);
+	*prev_pipe = fd[0];
+	waitpid(pid, NULL, 0);
 	free(expected);
-}
-
-int	open_outfile(char *path, int hdoc)
-{
-	int	fd;
-	int	flags;
-
-	if (hdoc)
-		flags = O_WRONLY | O_CREAT | O_APPEND;
-	else
-		flags = O_WRONLY | O_CREAT | O_TRUNC;
-	fd = open(path, flags, 0644);
-	if (fd < 0)
-		(perror("pipex: outfile"), exit(1));
-	return (fd);
 }
