e2fdfc09bc2814a6998cdfad029e02dea1309e7a
Youssef Mazini <maziniyoussef29@gmail.com>
2025-02-18 13:59:18 +0100

    again testing this version

--- src_bonus/pipex_bonus.h
+++ src_bonus/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ymazini <ymazini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 14:34:13 by ymazini           #+#    #+#             */
-/*   Updated: 2025/02/17 23:26:40 by ymazini          ###   ########.fr       */
+/*   Updated: 2025/02/18 13:58:49 by ymazini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@
 # include <sys/wait.h>
 # include <fcntl.h>  
 
-# define HDOC_LIMITER 2
-# define HDOC_CMD 3
+// check this
 
 pid_t	handle_multipipe(int ac, char **av, char **env, int hdoc);
 void	handle_hdoc(char *limiter, int *prev_pipe);
